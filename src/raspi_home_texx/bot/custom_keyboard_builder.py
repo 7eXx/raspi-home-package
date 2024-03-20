6d620d8b7b56f1a142e2a363430b97e1bfe13788
marco.tessari <marco.tessari@fv.lan>
2024-03-20 17:22:32 +0100

    fix: args for inline keyboard

--- src/raspi_home_texx/bot/custom_keyboard_builder.py
+++ src/raspi_home_texx/bot/custom_keyboard_builder.py
@@ -47,4 +47,4 @@ class CustomKeyboardBuilder:
             [self.ecu_check_btn, self.gate_check_btn]
         ]
 
-        return InlineKeyboardMarkup(keyboard=keyboard)
+        return InlineKeyboardMarkup(keyboard)

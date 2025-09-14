224524dd273653704e3d2da7b4dc5af9c885ae50
Texx <marco.tex@live.com>
2025-09-14 12:30:05 +0200

    feat: timestamp is available for environment in base class.

--- src/raspi_home_texx/environment_info.py
+++ src/raspi_home_texx/environment_info.py
@@ -4,6 +4,7 @@ class EnvironmentInfo(ABC):
 
     def __init__(self, status = "n/a"):
         self._status = status
+        self._timestamp = "n/a"
 
     @abstractmethod
     def serialize(self) -> str:
@@ -18,3 +19,9 @@ class EnvironmentInfo(ABC):
 
     def set_status(self, status) -> None:
         self._status = status
+
+    def get_timestamp(self) -> str:
+        return self._timestamp
+
+    def set_timestamp(self, timestamp) -> None:
+        self._timestamp = timestamp
